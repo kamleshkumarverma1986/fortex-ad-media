4c95d789875430597ac7ee20c1e8edde0977a61b
ns-kkumarverma <kkumarverma@netskope.com>
2025-11-04 01:15:37 +0530

    added more secure dashbaord

--- src/app/admin-dashboard/page.js
+++ src/app/admin-dashboard/page.js
@@ -3,6 +3,17 @@ import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import AdminDashboard from "@/components/AdminDashboard";
 
+// All these are protect this secure page even if user press back button
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+export const metadata = {
+  title: "Admin Dashboard",
+};
+export const headers = {
+  "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+};
+
 export default async function ServerAdminDashboard() {
   const session = await getServerSession(authOptions);
 
